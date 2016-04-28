61a933f105cad0a764417e1cf9cee9166df8926b
vikram.choudhary <vikram.choudhary@huawei.com>
2016-04-28 17:10:36 +0530

    Added alembic DB migration framework
    This patch adds alembic DB migration framework required by
    neutron-dynamic-routing repository for defining new data
    models to realize BGP dynamic routing functionality in neutron.
    
    Below changes are done as part of this patch-set:
     - Added new alembic migration framework.
     - PEP8 changes for alembic migration check.
    
    Change-Id: I395ca9541bceda40bbc0c0870e58c18231ba44e9
    Implements: blueprint bgp-spinout
    Partial-Bug: #1560003

---
+++ neutron_dynamic_routing/db/migration/alembic_migrations/versions/start_neutron_dynamic_routing.py
@@ -0,0 +1,30 @@
+#    Copyright 2016 Huawei Technologies India Pvt Limited.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""start neutron-dynamic-routing chain
+
+Revision ID: start_neutron_dynamic_routing
+Revises: None
+Create Date: 2016-04-26 18:42:08.262632
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'start_neutron_dynamic_routing'
+down_revision = None
+
+
+def upgrade():
+    pass
