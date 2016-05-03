fb4c68d6203c4ba0e6e3dc5b8309ed98ddd0057e
steve.ruan <ruansx@cn.ibm.com>
2016-05-03 07:26:15 -0500

    Add devstack for neutron-dynamic-routing stadium project
    Change-Id: I406b276f691e68adac935a4a2119efc055df31b1
    Implements: blueprint bgp-spinout
    Partial-Bug: #1560003

---
+++ devstack/plugin.sh
@@ -0,0 +1,23 @@
+LIBDIR=$NEUTRON_DYNAMIC_ROUTING_DIR/devstack/lib
+
+source $LIBDIR/dr
+
+if [[ "$1" == "stack" ]]; then
+    case "$2" in
+        install)
+            echo_summary "Installing neutron-dynamic-routing"
+            dr_configure
+            ;;
+        post-config)
+            echo_summary "Configuring neutron-dynamic-routing"
+            dr_post_configure
+            ;;
+        extra)
+            echo_summary "Launching neutron-dynamic-routing agent"
+            start_dr_agent
+            ;;
+    esac
+elif [[ "$1" == "unstack" ]]; then
+    echo_summary "Uninstalling neutron-dynamic-routing"
+    stop_dr_agent
+fi
